53ac7cc8a41097a81e20b141fbe8101e2cc1c7e4
leroyrosales <leroyrosales@gmail.com>
2019-11-02 07:04:21 -0500

    Adds index.php and leaflogix admin submenu

--- leaflogix-products.php
+++ leaflogix-products.php
@@ -11,3 +11,20 @@ Author: Leroy Rosales
 Author URI: https://leroyrosales.com/
 */
 
+add_action( "admin_menu", "leaflogix_admin_menu" );
+
+function leaflogix_admin_menu() {
+  add_submenu_page( 
+    "options-general.php", 
+    "Leaflogix API Settings", 
+    "Leaflogix API", 
+    "manage_options", 
+    "leaflogix-settings", 
+    "leaflogix_admin_func"
+  );
+}
+
+function leaflogix_admin_func(){
+  echo "<div class='wrap'><div id='dashicons-admin-network' class='icon32'><br></div>
+  <h2>Leaflogix API Settings</h2></div>";
+}
