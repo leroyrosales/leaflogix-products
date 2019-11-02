6e68e995ecd83a9b32a8e37a4b5255911f81fd48
leroyrosales <leroyrosales@gmail.com>
2019-11-02 06:35:59 -0500

    Adds leaflogix-products.php and plugin header info

---
+++ leaflogix-products.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Leaflogix Products
+ * @version 0.0.0
+ */
+/*
+Plugin Name: Leaflogix Products
+Description: WordPress plugin to create products listing via the Leaflogix API.
+Version: 0.0.0
+Author: Leroy Rosales
+Author URI: https://leroyrosales.com/
+*/
+
