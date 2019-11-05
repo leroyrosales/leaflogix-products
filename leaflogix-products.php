71d1ed743409299d681f5b957a7125b2d2058ef8
leroyrosales <leroyrosales@gmail.com>
2019-11-04 19:32:24 -0600

    Adds http get response function, wraps api key in base 64 encoding

--- leaflogix-products.php
+++ leaflogix-products.php
@@ -74,6 +74,12 @@ function leaflogix_settings_page_func() {
 	?>
 
     <div class="wrap">
+		<style>
+		pre {
+			width: 95%; overflow: auto; margin: 20px 0; padding: 20px;
+			color: #fff; background-color: #424242;
+		}
+	</style>
         <?php settings_errors();?>
         <form method="POST" action="options.php">
 		    <?php settings_fields( LEAFLOGIX_SETTINGS_PAGE_SLUG );?>
@@ -81,7 +87,68 @@ function leaflogix_settings_page_func() {
 		    <?php submit_button();?>
         </form>
         <div class="help-text"><em>For an API key contact <a href="https://leaflogix.com/" target="_blank">Leaflogix</a>. For documentation and testing you can test your API key via the <a href="http://leaflogix-publicapi.azurewebsites.net/swagger/" target="_blank">LeafLogixAPI documentation</a>.</em></div>
+
+				<?php echo http_get_products_response(); ?>
     </div>
     <?php
 
 }
+
+// GET response function
+function http_get_products_response() {
+
+	$api_key_text = base64_encode( esc_attr(get_option("leaflogix_swagger_api_key", "")) );
+
+	$args = array(
+		'method'      => 'GET',
+		'headers'     => array(
+			"Content-Type" => "application/json; charset=utf-8",
+			"Authorization" => "Basic " . $api_key_text
+		),
+	);
+
+	$url = esc_url_raw( 'http://leaflogix-publicapi.azurewebsites.net/products' );
+
+	$response = wp_safe_remote_get( $url, $args );
+
+	// response data
+	$code    = wp_remote_retrieve_response_code( $response );
+	$message = wp_remote_retrieve_response_message( $response );
+	$body    = wp_remote_retrieve_body( $response );
+	$headers = wp_remote_retrieve_headers( $response );
+
+	$header_date  = wp_remote_retrieve_header( $response, 'date' );
+	$header_type  = wp_remote_retrieve_header( $response, 'content-type' );
+	$header_cache = wp_remote_retrieve_header( $response, 'cache-control' );
+
+
+	// output data
+	$output  = '<h2><code>'. $url .'</code></h2>';
+
+	$output .= 'API KEY: <code>' . $api_key_text . '</code>';
+
+	$output .= '<h3>Status</h3>';
+	$output .= '<div>Response Code: '    . $code    .'</div>';
+	$output .= '<div>Response Message: ' . $message .'</div>';
+
+	$output .= '<h3>Body</h3>';
+	$output .= '<pre>';
+	ob_start();
+	var_dump( $body );
+	$output .= ob_get_clean();
+	$output .= '</pre>';
+
+	$output .= '<h3>Headers</h3>';
+	$output .= '<div>Response Date: ' . $header_date  .'</div>';
+	$output .= '<div>Content Type: '  . $header_type  .'</div>';
+	$output .= '<div>Cache Control: ' . $header_cache .'</div>';
+	$output .= '<pre>';
+	ob_start();
+	var_dump( $headers );
+	$output .= ob_get_clean();
+	$output .= '</pre>';
+
+	return $output;
+
+}
+
